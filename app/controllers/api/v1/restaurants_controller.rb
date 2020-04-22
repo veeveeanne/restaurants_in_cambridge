7a4a07049098ce9484c7f068f118a8f1823b5de7
jmzxhu <jmzcode15@gmail.com>
2020-04-22 16:34:01 -0400

    Added restaurant index feature

--- app/controllers/api/v1/restaurants_controller.rb
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -1,2 +1,5 @@
 class Api::V1::RestaurantsController < ApplicationController
+  def index
+    render json: Restaurant.all
+  end
 end
---
+++ app/javascript/react/components/RestaurantTile.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const RestaurantTile = (props) => {
+
+  return (
+    <div className="tile cell small-12 medium-4 large-4">
+      <img className="tile-image" src={props.restaurant.picture_url}/>
+      <div className="tile-text">{props.restaurant.name}</div>
+    </div>
+  )
+}
+export default RestaurantTile
