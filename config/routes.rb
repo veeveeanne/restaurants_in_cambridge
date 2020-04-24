39881f27b57cd2e694539604f370636cdf804d8e
Vivian Wang <59185503+veeveeanne@users.noreply.github.com>
2020-04-24 09:20:15 -0400

    Revert "Restaurant show feature"

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ Rails.application.routes.draw do
   root 'restaurants#index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  resources :restaurants, only: [:new, :create, :index, :show]
+  
+  resources :restaurants, only: [:new, :create, :index]
 
   namespace :api do
     namespace :v1 do
-      resources :restaurants, only: [:index, :show]
+      resources :restaurants, only: [:index]
     end
   end
 end
