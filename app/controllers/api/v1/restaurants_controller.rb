5030ae3509e8e18469982d04ebfab3ff5b4fef4b
Vivian Wang <vivian.emmenuel.wang@gmail.com>
2020-04-24 11:40:22 -0400

    fixing Restaurant show feature

--- app/controllers/api/v1/restaurants_controller.rb
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -2,4 +2,8 @@ class Api::V1::RestaurantsController < ApplicationController
   def index
     render json: Restaurant.all
   end
+
+  def show
+    render json: Restaurant.find(params[:id])
+  end
 end
