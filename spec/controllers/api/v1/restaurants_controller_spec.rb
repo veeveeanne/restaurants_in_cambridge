2514f40d8f3ecb1d8f444cef85b274a4653b1e7d
Aidan Harris <Ahharris117@gmail.com>
2020-04-27 12:53:03 -0400

    updated tests, user role: admin

--- spec/controllers/api/v1/restaurants_controller_spec.rb
+++ spec/controllers/api/v1/restaurants_controller_spec.rb
@@ -37,7 +37,8 @@ RSpec.describe Api::V1::RestaurantsController, type: :controller do
 
   describe "GET#show" do
     let!(:restaurant_one) { Restaurant.create(name: "Mr. Bartley", address: "1245 Massachusetts Ave", city: "Cambridge", state: "MA", zip: "02138", picture_url: "https://s3-media0.fl.yelpcdn.com/bphoto/UIDbWpmwnCBi9Hvp0QJOHw/o.jpg") }
-
+    let!(:user_one) { User.create(email: "a1@b.com", screen_name: "User1234", password: "password") }
+  
     it "returns a successful response status and a content type of json" do
       get :show, params: {id: restaurant_one.id}
 
@@ -46,17 +47,24 @@ RSpec.describe Api::V1::RestaurantsController, type: :controller do
     end
 
     it "returns a restaurant's information in the data base" do
+      sign_in user_one
       get :show, params: {id: restaurant_one.id}
 
       response_body = JSON.parse(response.body)
+      user_response = response_body[0]
+      restaurant_response = response_body[1]
+      
+      expect(response_body.length).to equal 2
+      expect(restaurant_response["name"]).to eq restaurant_one.name
+      expect(restaurant_response["address"]).to eq restaurant_one.address
+      expect(restaurant_response["city"]).to eq restaurant_one.city
+      expect(restaurant_response["state"]).to eq restaurant_one.state
+      expect(restaurant_response["zip"]).to eq restaurant_one.zip
+      expect(restaurant_response["picture_url"]).to eq restaurant_one.picture_url
 
-      expect(response_body.length).to equal 10
-      expect(response_body["name"]).to eq restaurant_one.name
-      expect(response_body["address"]).to eq restaurant_one.address
-      expect(response_body["city"]).to eq restaurant_one.city
-      expect(response_body["state"]).to eq restaurant_one.state
-      expect(response_body["zip"]).to eq restaurant_one.zip
-      expect(response_body["picture_url"]).to eq restaurant_one.picture_url
+      expect(user_response["email"]).to eq user_one.email
+      expect(user_response["screen_name"]).to eq user_one.screen_name
+      expect(user_response["role"]).to eq user_one.role
     end
   end
 end
