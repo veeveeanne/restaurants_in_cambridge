02d6630b8fa533bbfef6a839b3b7ad26ae4d9136
Aidan Harris <Ahharris117@gmail.com>
2020-04-28 09:48:43 -0400

    fixed aws bucket name

--- app/uploaders/profile_photo_uploader.rb
+++ app/uploaders/profile_photo_uploader.rb
@@ -9,13 +9,13 @@ class ProfilePhotoUploader < CarrierWave::Uploader::Base
   else
     storage :fog
   end
- 
+
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
-  
+
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
   #   # For Rails 3.1+ asset pipeline compatibility:
